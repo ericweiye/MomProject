751fa53d331dec318dc96f3d9782eac7f69dd03f
ericweiye <eric_weiye@live.com>
2014-05-18 12:16:14 +0800

    house keep add all files in

---
+++ 11储罐加热计算/11储罐加热计算/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace _11储罐加热计算
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.B7 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.B8 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.B9 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.B11 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.B17 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.B67 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.B18 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.B30 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.B5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.min52 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.min120 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // B4
+            // 
+            this.B4.Location = new System.Drawing.Point(12, 12);
+            this.B4.Name = "B4";
+            this.B4.Size = new System.Drawing.Size(100, 21);
+            this.B4.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "4:tbe——油品加热始温，℃";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "7:γTγop——油品的密度，t/m3";
+            // 
+            // B7
+            // 
+            this.B7.Location = new System.Drawing.Point(12, 66);
+            this.B7.Name = "B7";
+            this.B7.Size = new System.Drawing.Size(100, 21);
+            this.B7.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "8:Tγ——油品密度下的温度，℃";
+            // 
+            // B8
+            // 
+            this.B8.Location = new System.Drawing.Point(12, 93);
+            this.B8.Name = "B8";
+            this.B8.Size = new System.Drawing.Size(100, 21);
+            this.B8.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(139, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "9:t1——粘度νt1op时对应的温度，℃";
+            // 
+            // B9
+            // 
+            this.B9.Location = new System.Drawing.Point(12, 120);
+            this.B9.Name = "B9";
+            this.B9.Size = new System.Drawing.Size(100, 21);
+            this.B9.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(139, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(215, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "11;t2——粘度νt2op时对应的温度，℃";
+            // 
+            // B11
+            // 
+            this.B11.Location = new System.Drawing.Point(12, 147);
+            this.B11.Name = "B11";
+            this.B11.Size = new System.Drawing.Size(100, 21);
+            this.B11.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(139, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "17:H——罐壁高度，m";
+            // 
+            // B17
+            // 
+            this.B17.Location = new System.Drawing.Point(12, 174);
+            this.B17.Name = "B17";
+            this.B17.Size = new System.Drawing.Size(100, 21);
+            this.B17.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(139, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 12);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "18:x——充装系数";
+            // 
+            // B67
+            // 
+            this.B67.Location = new System.Drawing.Point(12, 255);
+            this.B67.Name = "B67";
+            this.B67.Size = new System.Drawing.Size(100, 21);
+            this.B67.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(139, 231);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(305, 12);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "30:tai——油品所在地区历年一月份平均温度平均值，℃";
+            // 
+            // B18
+            // 
+            this.B18.Location = new System.Drawing.Point(12, 201);
+            this.B18.Name = "B18";
+            this.B18.Size = new System.Drawing.Size(100, 21);
+            this.B18.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(139, 258);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(149, 12);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "67:g——重力加速度，m/s2";
+            // 
+            // B30
+            // 
+            this.B30.Location = new System.Drawing.Point(12, 228);
+            this.B30.Name = "B30";
+            this.B30.Size = new System.Drawing.Size(100, 21);
+            this.B30.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(139, 42);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(155, 12);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "5:ten——油品加热终温，℃";
+            // 
+            // B5
+            // 
+            this.B5.Location = new System.Drawing.Point(12, 39);
+            this.B5.Name = "B5";
+            this.B5.Size = new System.Drawing.Size(100, 21);
+            this.B5.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "计算 第52行最小值";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(403, 36);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(182, 45);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "When twc——罐壁推算温度（先假设略小于tav的值试算）,℃ (line 52) =";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // min52
+            // 
+            this.min52.AutoSize = true;
+            this.min52.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.min52.Location = new System.Drawing.Point(467, 81);
+            this.min52.Name = "min52";
+            this.min52.Size = new System.Drawing.Size(64, 16);
+            this.min52.TabIndex = 22;
+            this.min52.Text = "label12";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(405, 102);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(107, 12);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "第120行绝对值最小";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(405, 163);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(83, 12);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "120行最小值为";
+            // 
+            // min120
+            // 
+            this.min120.AutoSize = true;
+            this.min120.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.min120.Location = new System.Drawing.Point(407, 189);
+            this.min120.Name = "min120";
+            this.min120.Size = new System.Drawing.Size(64, 16);
+            this.min120.TabIndex = 25;
+            this.min120.Text = "label14";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 307);
+            this.Controls.Add(this.min120);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.min52);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.B5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.B30);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.B18);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.B67);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.B17);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.B11);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.B9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.B8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.B7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.B4);
+            this.Name = "Form1";
+            this.Text = "120行最小值计算";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox B4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox B7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox B8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox B9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox B11;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox B17;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox B67;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox B18;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox B30;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox B5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label min52;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label min120;
+    }
+}
+
